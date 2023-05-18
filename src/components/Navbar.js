5739b14cb7738ab22a259b356f0890afe859e5f6
Sharad Kevadiya <64027328+sharad-999@users.noreply.github.com>
2023-05-18 13:02:02 +0530

    removed restrict mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <Link class="navbar-brand" to="/">User Management</Link>
+                <Link class="navbar-brand" to="/">staff Management</Link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
